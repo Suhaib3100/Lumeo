b2e351e8697ca66bbe2ff9dbd4cb5c298c23a641
Suhaib King <suhaibking310@gmail.com>
2025-03-01 16:11:10 +0530

    iniated github sigins

--- components/signup-form-demo.jsx
+++ components/signup-form-demo.jsx
@@ -10,7 +10,7 @@ import {
   IconBrandGoogle,
   IconBrandOnlyfans,
 } from "@tabler/icons-react";
-
+import { signIn } from 'next-auth/react';
 export default function SignupFormDemo() {
   const [formData, setFormData] = React.useState({
     firstname: "",
@@ -174,10 +174,15 @@ export default function SignupFormDemo() {
         <div className="flex flex-col space-y-4">
           <button
             className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-            type="submit">
+            type="button"
+            onClick={() => signIn('github', { 
+              callbackUrl: '/',
+              redirect: true
+            })}
+          >
             <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
             <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-              GitHub
+              Continue with GitHub
             </span>
             <BottomGradient />
           </button>
